f7be311d2aac4553234efb75bbcd5a52af4a5c08
AndyG1228 <606925@student.dusd.net>
2022-11-16 11:45:14 -0800

    GameObject Collision
    Updated Ruby's movement to have collision and RigidBody, and added collision to the MetalBox as well.

--- RubysAdventureGallardoP3/Assets/Scripts/RubyController.cs
+++ RubysAdventureGallardoP3/Assets/Scripts/RubyController.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        //New Variable
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Creating the two variables that use the pre built axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+    }
 
+     void FixedUpdate()
+    {
         //Create the movement vector
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
 
         //Changing the x and y position
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         //Set the updated position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
